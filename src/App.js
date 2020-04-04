4caf85c24167a44e2aff2b2a8f25d3c7f4537b8b
Miguel <mike.rodriguez91@gmail.com>
2020-04-04 03:29:30 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./index.css";
+ReactDOM.render(
+	<div className="bg-red-500">hello world from React! </div>,
+	document.getElementById("root")
+);
